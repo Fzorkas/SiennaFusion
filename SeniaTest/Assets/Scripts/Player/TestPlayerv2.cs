22ff821cbdc06b61f941512518301f23a02fabb0
CodeingerCat <DBbombed@Gmail.com>
2018-04-02 01:19:48 -0500

    Clean up/ Text Box
    + Added Custom Font
    + Added PreFab of Textbox
    + Added Text Box Controll Scripts
    > Functions Include
        > SetDisplay = Open/Change Disply format type and text/options in it
        > StopDisplay = Closes the Open Display
        > Varius Internal function to Run typing and word wraping
        > Hit Space to make text go faster (Note: key will be added to custom key system when added <is in development>)
    + Organized all the folders

---
+++ SeniaTest/Assets/Scripts/Player/TestPlayerv2.cs
@@ -0,0 +1,198 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerv2 : MonoBehaviour
+{
+    public TextBoxTyper testTextBox;
+
+    // in editor inputs
+    public Vector2 offset;
+    public GameObject baseGrassObject;
+    public Sprite grassImage1, grassImage2, grassImage3;
+
+    // variables to be set at start
+    private Animator animator; // animator for the player
+    private float transitionTime; // base time to move from one tile to the next
+    private GameObject grass1, grass2, grassTop1, grassTop2; // grass efects for going throgh tall grass
+
+    // internal use variables
+        //memoty between frames
+        private bool moveing = false;
+        private Vector3 remPosition;
+        private float totalTime;
+
+	void Start () {
+        animator = gameObject.GetComponent<Animator>();
+
+        transitionTime = 0.2f;
+
+        transform.position = new Vector2(Mathf.RoundToInt(transform.position.x - offset.x), Mathf.RoundToInt(transform.position.y-offset.y)) + offset;
+    }
+
+    void Update()
+    {
+        if (moveing)
+        {
+            PosAnimate();
+        }
+        else
+        {
+            moveing = PlayerDetect.InputToDirection();
+            //Debug.Log(PlayerDetect.direction);
+            animator.SetInteger("Direction", PlayerDetect.direction);
+            if(moveing)
+            {
+                remPosition = transform.position;
+                moveing = true;
+
+                moveing = PlayerDetect.Wall(transform.position);
+                if (moveing)
+                {
+                    /*string[] strings = new string[] { "Hello this is a test message to see if the text typer is running corectily",
+                        "Testing Testing one two three dose this text wraping work corectly at all",
+                        "yep here is another test mesage to test the text wraping system"};
+                    testTextBox.SetDispley("test", 0, strings[Random.Range(0,3)]);*/
+                    PlayerDetect.Grass(ref baseGrassObject, ref grass2, grassImage1, grassImage2, grassImage3);
+                } 
+            }
+        }
+    }
+
+    private void PosAnimate()
+    {
+        totalTime += Time.deltaTime;
+        if (baseGrassObject.activeSelf == true)
+        {
+            baseGrassObject.GetComponent<Animator>().SetBool("Moveing", false);
+        }
+
+        if(totalTime > transitionTime)
+        {
+            totalTime = 0;
+            transform.position = remPosition + (Vector3)PlayerDetect.dirVec;
+            moveing = false;
+        }
+        else
+        {
+            transform.position += (Vector3)(PlayerDetect.dirVec * (Time.deltaTime/transitionTime));
+            moveing = true;
+        }
+    }
+}
+
+public static class PlayerDetect
+{
+    // key inputs to a Direction value (0 = no mvoement, 1 = up(y), 2 = right(x), 3 = down(-y), 4 = left(-x))
+    public static int direction = 0;
+    public static Vector2 dirVec = new Vector2(0, 0);
+
+    public static bool InputToDirection()
+    {
+        // get unit value for x
+        int dx = 0;
+        if (Input.GetKey(KeyCode.RightArrow))
+            dx = 1;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            dx += -1;
+
+        // get unit value for y
+        int dy = 0;
+        if (Input.GetKey(KeyCode.UpArrow))
+            dy = 1;
+        if (Input.GetKey(KeyCode.DownArrow))
+            dy += -1;
+
+
+        // Traslate unit values and privius movement direction into new direction value and vector
+        if (dy != 0 && dx != 0)
+        {
+            // figures out wich direction to move on diagnals
+            if ((direction == 0) || (direction == 1) || (direction == 3))
+            {
+                // last movement in y --> now move in x
+                direction = (-dx + 3);
+                dirVec = new Vector2(dx, 0);
+            }
+            else if ((direction == 2) || (direction == 4))
+            {
+                // last movement in x --> now move in y
+                direction = (-dy + 2);
+                dirVec = new Vector2(0, dy);
+            }
+            return true;
+        }
+        else if (dy != 0)
+        {
+            // up/down movement calculations
+            direction = (-dy + 2);
+            dirVec = new Vector2(0, dy);
+            return true;
+        }
+        else if (dx != 0)
+        {
+            // right/left movement calculations
+            direction = (-dx + 3);
+            dirVec = new Vector2(dx, 0);
+            return true;
+        }
+        else if (direction != 0)
+        {
+            direction = 0;
+            dirVec = new Vector2(0, 0);
+        }
+
+        return false;
+    }
+
+    // detection of suronding trigers and walls
+    public static GameObject par;
+    public static GameObject grass;
+    public static Vector2 positon;
+    public static Vector2 front;
+    public static bool grassOn;
+    public static bool wall;
+
+    public static bool Wall(Vector2 Position)
+    {
+        positon = Position;
+        front = positon + dirVec;
+
+        Collider2D t1 = Physics2D.OverlapPoint(front);
+        if (t1)
+        {
+            wall = t1.isTrigger;
+            return wall;
+        }
+
+        wall = true;
+        return true;
+    }
+
+    public static bool Grass(ref GameObject g1, ref GameObject g2, Sprite im1, Sprite im2, Sprite im3)
+    {
+        Vector2 frontDown = front + Vector2.down;
+
+        bool t0 = Physics2D.OverlapPoint(positon, 256);
+        Collider2D t1 = Physics2D.OverlapPoint(front, 256);
+        Collider2D t2 = Physics2D.OverlapPoint(frontDown, 256);
+
+        if (t1 || t0)
+        {
+            if (t1)
+            {
+                g1.SetActive(true);
+                g1.GetComponent<Animator>().SetBool("Moveing", true);
+            }
+            else if (t0 && wall)
+            {
+                g1.SetActive(false);
+            }
+        }
+        else
+        {
+            g1.SetActive(false);
+        }
+        return t1;
+    }
+}
---
+++ SeniaTest/Assets/Scripts/UI/TextBoxTyper.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxTyper : MonoBehaviour {
+    // [[ Variables ]]
+    // pointers to display pices
+    Transform display; //Holds all parts of text box display.
+    Text bodyText1; //Difrent body Text part types in the text box.
+    Text Label1; //Difrent label Text part tyos in the text box.
+
+    // refrences for curent displat
+    Text curText; //The current Text field to be used by the Typer.
+    Text curTitle; // The current Test Fild fot the Box title
+    string curString; //The curent Message to be typed.
+
+    // typer refrence values
+    public bool typeing = false; // used to turn on and off typing
+    int curWordPos; //The end position of current word ins curString.
+    int curLineAdvance; // total length of all words and spaces of a line
+    string curWord = ""; //The currrent word being typed out
+    int curWordAdvance; //Lenght of current word in pixles
+    int curInternalWordPos; //The position in the curent word that is to be typed
+    bool hyperspeed = false;
+
+    // [[ Functions ]]
+    public void OnEnable()
+    {
+        display = transform.Find("Display");
+        display.gameObject.SetActive(false);
+
+        bodyText1 = display.Find("FullBody").GetComponent<Text>();
+        bodyText1.enabled = false;
+        Label1 = display.Find("Label1").GetComponent<Text>();
+        Label1.enabled = false;
+
+        curText = bodyText1;
+        curTitle = Label1;
+    }
+
+    // Open/Change Disply type and title
+    public void SetDispley(string title, int type, string[] Content)
+    {
+        curText.enabled = false;
+        curTitle.enabled = false;
+        display.gameObject.SetActive(true);
+
+        if (type == 0)
+            SetupTyper(bodyText1, Content[0], Label1, title);
+    }
+    public void SetDispley(string title, int type, string content)
+    {
+        SetDispley(title, type, new string[] { content });
+    }
+
+    // Close curently active disply
+    public void StopDispley()
+    {
+        display.gameObject.SetActive(false);
+    }
+
+    // grabs next word and length of it after curWordPos in curString
+    public void wordGrab()
+    {
+        curWord = "";
+        curWordAdvance = 0;
+        CharacterInfo characterInfo;
+        while (curWordPos <= curString.Length - 1 && !char.IsSeparator(curString[curWordPos]))
+        {
+            curText.font.GetCharacterInfo(curString[curWordPos], out characterInfo);
+            curWordAdvance += characterInfo.advance;
+            curWord += curString[curWordPos];
+            curWordPos++;
+        }
+        curWordPos++;
+    }
+
+    // sets up variables for the typer
+    public void SetupTyper(Text bodyTextField, string text, Text labelTextField, string label)
+    {
+        // set up displayes label
+        curTitle = labelTextField;
+        curTitle.text = label;
+        curTitle.enabled = true;
+
+        // set up displays text box / typer resorces
+        curText = bodyTextField;
+        curText.text = "";
+        curText.enabled = true;
+        curString = text;
+        curWordPos = 0;
+        curWord = "";
+        curLineAdvance = 0;
+        curInternalWordPos = 0;
+        hyperspeed = false;
+
+        //turn typing on
+        typeing = true;
+    }
+
+    // types leters bases on fix updates
+    public void FixedUpdate()
+    {
+        if (typeing)
+        {
+            if (curInternalWordPos == curWord.Length) //Test for word to have ended on last update
+            {
+                if (curWordPos >= curString.Length) //Test end of string so as to staop typing
+                {
+                    typeing = false;
+                    return;
+                }
+                if(curLineAdvance != 0) //Test for not fists word since only the first word starts with a curLineAdvance of 0
+                {
+                    curText.text += " ";
+                    CharacterInfo characterInfo;
+                    curText.font.GetCharacterInfo(" "[0], out characterInfo);
+                    curLineAdvance += characterInfo.advance;
+                }
+                wordGrab();
+                curLineAdvance += curWordAdvance;
+                curInternalWordPos = 0;
+                if (curLineAdvance >= curText.rectTransform.rect.size.x) //Test to see if adding new word exedes line length
+                {
+                    curText.text += Environment.NewLine;
+                    curLineAdvance = curWordAdvance;
+                }
+            }
+            else if (hyperspeed) //Test for speed boost to dialog that types whole words at a time
+            {
+                for(int i = curInternalWordPos; i < curWord.Length; i++)
+                {
+                    curText.text += curString[(curWordPos + i) - (curWord.Length + 1)];
+                }
+                curInternalWordPos = curWord.Length;
+            }
+            else // typing mode so types leters
+            {
+                curText.text += curString[curWordPos + curInternalWordPos - (curWord.Length + 1)];
+                curInternalWordPos++;
+            }
+        }
+    }
+
+    public void Update() // just here to detect key for hyperspeed mode
+    {
+        if(typeing && Input.GetKeyDown(KeyCode.Space)) // note: hook space key into controlls later
+        {
+            hyperspeed = true;
+        }
+    }
+}
